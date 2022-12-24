1671acfa285d788a873fabbddd216a0ca24c1cae
A.Salieri <eon_rossa@mail.ru>
2022-12-24 20:43:50 +0300

    Попытка номер 3

--- Source.cpp
+++ Source.cpp
@@ -5,16 +5,28 @@
 int main() {
  //Написать программу, которая из имеющегося массива строк формирует массив строк, длина которых меньше или равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами. 
 {
-char arr[100];
-char arrar[100];
-char arrarar[100];
+int main() {
+  char arr[100];
+  char arr2[100];
+  char arr3[100];
   std::cout << ("Введите символы: ") << std::endl;
   std::cin >> arr;
   std::cout << ("Введите символы: ") << std::endl;
-  std::cin >> arrar;
+  std::cin >> arr2;
   std::cout << ("Введите символы: ") << std::endl;
-  std::cin >> arrarar;
-  printf ("The sentence entered is %u characters long.\n",(unsigned)strlen(arr));
+  std::cin >> arr3;
+  if (strlen(arr) <= 3) {
+    printf("\nДлина массива не более 3 символов.\n");
+    printf(arr);
+  };
+  if (strlen(arr2) <= 3) {
+    printf("\nДлина массива не более 3 символов.\n");
+    printf(arr2);
+  }
+  if (strlen(arr3) <= 3) {
+    printf("\nДлина массива не более 3 символов.\n");
+    printf(arr3);
+  }
   return 0;
 
 }
