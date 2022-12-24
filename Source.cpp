ca1151daa724881447a304e13a3383f917675fbc
A.Salieri <eon_rossa@mail.ru>
2022-12-24 20:26:54 +0300

    Попытка 2

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
 
 int main() {
  //Написать программу, которая из имеющегося массива строк формирует массив строк, длина которых меньше или равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами. 
-char a ; 
-char b; 
-char c; 
-std::cout << 'Введите символы с клавиатуры: ' >> std::endl;
-std::cin >> a; 
-std::cin >> b; 
-std::cin >> c; 
+{
+char arr[100];
+char arrar[100];
+char arrarar[100];
+  std::cout << ("Введите символы: ") << std::endl;
+  std::cin >> arr;
+  std::cout << ("Введите символы: ") << std::endl;
+  std::cin >> arrar;
+  std::cout << ("Введите символы: ") << std::endl;
+  std::cin >> arrarar;
+  printf ("The sentence entered is %u characters long.\n",(unsigned)strlen(arr));
+  return 0;
 
 }
+
+
+
+
